8599d5d9a392b1128e73a4a72deae86d80750747
Silvian-73 <yurykuz65@gmail.com>
2021-03-01 22:07:13 +0300

    Making database interaction

---
+++ Dota-2-50-percent-study/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Dota_2_50_percent_study
+{
+    public static class Database
+    {
+        public static string Username;
+        public static string Password;
+
+        private static MySqlConnection _connection;
+
+        public static void Connect()
+        {
+            string connStr = $"server=localhost;user={Username};database=dota_2_50_percent_study;password={Password};";
+            _connection = new MySqlConnection(connStr);
+            _connection.Open();
+        }
+
+        public static void AddMatches(params Match[] matches)
+        {
+            StringBuilder commandStr = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                commandStr.Append(
+                    $"INSERT IGNORE INTO matches (id, match_sequence_number, winner) VALUES ({match.Id}, {match.MatchSequenceNumber}, {(int) match.Winner});\n");
+                foreach (var player in match.DirePlayers)
+                {
+                    commandStr.Append(
+                        $"INSERT INTO players (id, match_id, team) VALUES ({player.Id}, {player.Match.Id}, {(int) Team.Dire});");
+                }
+
+                foreach (var player in match.RadiantPlayers)
+                {
+                    commandStr.Append(
+                        $"INSERT INTO players (id, match_id, team) VALUES ({player.Id}, {player.Match.Id}, {(int) Team.Radiant});");
+                }
+            }
+            MySqlCommand command = new MySqlCommand(commandStr.ToString(), _connection);
+            int rowsAffected = command.ExecuteNonQuery();
+            Console.WriteLine($"SQL command executed, {rowsAffected} rows affected");
+        }
+    }
+}
--- Dota-2-50-percent-study/Program.cs
+++ Dota-2-50-percent-study/Program.cs
@@ -8,40 +8,90 @@ namespace Dota_2_50_percent_study
     internal class Program
     {
         private const string KeyPath = "key.txt";
+        private const string DBUserNamePath = "dbusername.txt";
+        private const string DBPasswordPath = "dbpassword.txt";
         
         public static void Main(string[] args)
         {
             if (!File.Exists(KeyPath))
             {
-                Console.Write("Enter your Steam Web API Key. You can get it here: https://steamcommunity.com/dev/apikey\n>");
+                Console.WriteLine("Enter your Steam Web API Key. You can get it here: https://steamcommunity.com/dev/apikey");
                 string key = Console.ReadLine();
                 File.WriteAllText(KeyPath, key);
+                Console.WriteLine();
             }
             WebAPI.Key = File.ReadAllText(KeyPath);
-
-            var matches = WebAPI.GetMatches(500, 4866994034);
             
-            ConsoleTable table = new ConsoleTable("Match", "Radiant players", "Dire players");
-            foreach (var match in matches)
+            if (!File.Exists(DBUserNamePath))
             {
-                StringBuilder radiantPlayers = new StringBuilder();
-                foreach (Player player in match.RadiantPlayers)
-                {
-                    radiantPlayers.Append(player.Id);
-                    radiantPlayers.Append(" ");
-                }
-                StringBuilder direPlayers = new StringBuilder();
-                foreach (Player player in match.DirePlayers)
-                {
-                    direPlayers.Append(player.Id);
-                    direPlayers.Append(" ");
-                }
-                table.AddRow(match.Id, radiantPlayers, direPlayers);
+                Console.WriteLine("Enter your MySQl database username");
+                string username = Console.ReadLine();
+                File.WriteAllText(DBUserNamePath, username);
+                Console.WriteLine();
             }
-            table.Write();
+            Database.Username = File.ReadAllText(DBUserNamePath);
+            
+            if (!File.Exists(DBPasswordPath))
+            {
+                Console.WriteLine("Enter your MySQl database password");
+                string password = Console.ReadLine();
+                File.WriteAllText(DBPasswordPath, password);
+                Console.WriteLine();
+            }
+            Database.Password = File.ReadAllText(DBPasswordPath);
 
-            Console.Write("Press any button to quit...");
-            Console.ReadKey();
+            string input = String.Empty;
+            while (input != "q")
+            {
+                Console.WriteLine(@"
+Choose subroutine:
+1 - add more matches to the database
+q - quit
+");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1" :
+                        AddMoreMatchesToTheDatabase();
+                        break;
+                }
+            }
+        }
+
+        public static void AddMoreMatchesToTheDatabase()
+        {
+            {
+                Console.WriteLine("How many matches to add?");
+                int amount = int.Parse(Console.ReadLine());
+                
+                var matches = WebAPI.GetMatches(100, 4866994034);
+                
+                Database.Connect();
+                Database.AddMatches(matches[0]);
+
+                #region DrawTable
+
+                // ConsoleTable table = new ConsoleTable("Match", "Radiant players", "Dire players");
+                // foreach (var match in matches)
+                // {
+                //     StringBuilder radiantPlayers = new StringBuilder();
+                //     foreach (Player player in match.RadiantPlayers)
+                //     {
+                //         radiantPlayers.Append(player.Id);
+                //         radiantPlayers.Append(" ");
+                //     }
+                //     StringBuilder direPlayers = new StringBuilder();
+                //     foreach (Player player in match.DirePlayers)
+                //     {
+                //         direPlayers.Append(player.Id);
+                //         direPlayers.Append(" ");
+                //     }
+                //     table.AddRow(match.Id, radiantPlayers, direPlayers);
+                // }
+                // table.Write();
+
+                #endregion
+            }
         }
     }
 }
